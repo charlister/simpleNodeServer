32965e48ca5c984051bde3da2d7275c09d8bb840
charlister <charlister22@gmail.com>
2022-03-21 17:48:57 +0100

    basic server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const port = process.env.PORT || 3000;
+const data = require("./data.json");
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+app.get('/membres', (req, res) => {
+    res.send(data);
+});
+
+app.listen(port, () => console.log(`listening on http://localhost:${port}`));
